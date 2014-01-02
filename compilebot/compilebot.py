8117cbf9b2dd3e87590a66c9d1ce29d3583b031b
Renfred Harper <renfredxh@gmail.com>
2014-01-01 19:35:56 -0500

    Edit error postamble

--- compilebot/compilebot.py
+++ compilebot/compilebot.py
@@ -400,8 +400,8 @@ BANNED_USERS = set()
 # Text
 ERROR_PREAMBLE = "There was an error processing your comment: {link}\n\n"
 ERROR_POSTAMBLE = ("You can edit your original comment and have it "
-                  "recompiled by replying to this message with the following "
-                  "command:\n\n--recompile {link}")
+                  "recompiled by replying to this message with the following: "
+                  "\n\n--recompile {link}")
 HELP_TEXT = SETTINGS['help_text']
 ERROR_TEXT = SETTINGS['error_text']
 # Spam Settings
