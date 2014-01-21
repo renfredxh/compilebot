36bbfbd38b320c6d4dea89701554ff5d71d5efdd
Renfred Harper <renfredxh@gmail.com>
2014-01-20 22:06:18 -0500

    Remove admin alerts for formatting errors

--- compilebot/compilebot.py
+++ compilebot/compilebot.py
@@ -258,7 +258,7 @@ def create_reply(comment):
         postamble = ERROR_POSTAMBLE.format(link=comment.permalink)
         error_text = preamble + FORMAT_ERROR_TEXT + postamble
         log("Formatting error on comment {c.permalink}:\n\n{c.body}".format(
-            c=comment), alert=True)
+            c=comment))
         return MessageReply(error_text)
     # Seperate the language name from the rest of the supplied options.
     try:
