6511d74438fda5fcaea6ccddf2853061aaad6e98
Renfred Harper <renfredxh@gmail.com>
2014-08-12 22:51:13 -0400

    Fix connection error issues

--- compilebot/deploy.py
+++ compilebot/deploy.py
@@ -1,6 +1,6 @@
 import time
 import traceback
-from requests import HTTPError
+from requests import HTTPError, ConnectionError
 import compilebot as bot
 
 SLEEP_TIME = 60
@@ -14,6 +14,9 @@ def main():
             except HTTPError as e:
                 # HTTP Errors may indicate reddit is overloaded.
                 # Sleep for some extra time. 
+                bot.log(str(e) + " ")
+                time.sleep(SLEEP_TIME*2)
+            except ConnectionError as e:
                 bot.log(str(e) + " ")
                 time.sleep(SLEEP_TIME*2)
             except Exception as e:
