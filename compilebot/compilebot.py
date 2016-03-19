cd0ac327a67e6a5a1cbc4232edf1f983a52e407b
Renfred Harper <renfredxh@gmail.com>
2016-03-19 12:39:23 -0700

    Stop logging full comment text

--- compilebot/compilebot.py
+++ compilebot/compilebot.py
@@ -309,7 +309,7 @@ def create_reply(comment):
         preamble = ERROR_PREAMBLE.format(link=comment.permalink)
         postamble = ERROR_POSTAMBLE.format(link=comment.permalink)
         error_text = preamble + FORMAT_ERROR_TEXT + postamble
-        log("Formatting error on comment {c.permalink}:\n\n{c.body}".format(
+        log("Formatting error on comment {c.permalink}".format(
             c=comment))
         return MessageReply(error_text)
     # Seperate the language name from the rest of the supplied options.
